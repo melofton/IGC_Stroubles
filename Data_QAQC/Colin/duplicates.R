776f333e85782c2f1fcd53f39cf5b05cd149ff2a
Colin Baciocco <tophat107@gmail.com>
2019-07-02 09:16:58 -0400

    First Colin Commit:
    Extended QAQC code to further clean data, calculate discharge, and merge WQ and discharge into one sheet.

---
+++ Data_QAQC/Colin/duplicates.R
@@ -0,0 +1,15 @@
+#This file contains methods to examine duplicate rows in WQ and stage
+B1_QA_duplicates <- arrange( filter( B1_QA, duplicated(B1_QA$DateTime) | duplicated(B1_QA$DateTime, fromLast = TRUE)),
+         DateTime)
+
+
+filter( B1_QA_duplicates, 
+        duplicated( B1_QA_duplicates[colnames(B1_QA_duplicates[-1])] ) | duplicated( B1_QA_duplicates[colnames(B1_QA_duplicates[-1])], fromLast = TRUE )
+) %>%
+  arrange(DateTime)
+
+
+#A quick visual scan of the table below, which contains ALL the duplicate rows for stage, shows that
+#there are no differences between duplicated rows greater than a millimeter
+B1_stage_QA_duplicates <- arrange( filter( B1_stage_QA, duplicated(B1_stage_QA$DateTime) | duplicated(B1_stage_QA$DateTime, fromLast = TRUE)),
+                             DateTime)
