429608e10d8b679f8d6fae5d65eb71ff2bebcd13
Colin Baciocco <tophat107@gmail.com>
2019-08-02 21:39:01 -0400

    Add document explaining QAQC pipeline and Colin vis code to root

--- Data_QAQC/Colin/duplicates.R
+++ Data_QAQC/Colin/duplicates.R
@@ -1,10 +1,12 @@
 #This file contains methods to examine duplicate rows in WQ and stage
+#It was written to ensure that dropping what seemed to be duplicate rows in the unQAQC-ed dataset by using distinct() would be kosher
 B1_QA_duplicates <- arrange( filter( B1_QA, duplicated(B1_QA$DateTime) | duplicated(B1_QA$DateTime, fromLast = TRUE)),
          DateTime)
 
 
 filter( B1_QA_duplicates, 
-        duplicated( B1_QA_duplicates[colnames(B1_QA_duplicates[-1])] ) | duplicated( B1_QA_duplicates[colnames(B1_QA_duplicates[-1])], fromLast = TRUE )
+        duplicated( B1_QA_duplicates[colnames(B1_QA_duplicates[-1])] ) |
+          duplicated( B1_QA_duplicates[colnames(B1_QA_duplicates[-1])], fromLast = TRUE )
 ) %>%
   arrange(DateTime)
 
--- Visualize/ANOVA_Data_Prep_CAB.R
+++ Visualize/ANOVA_Data_Prep_CAB.R
@@ -1,3 +1,4 @@
+#This saves a .csv that was destined for Vasiliy
 #Environmental Dependencies: B1 and filt_storms_info which work for poster_visuals
 
 #Record to filt_storms_info:
@@ -6,17 +7,29 @@
 #3. maximum discharge seen in-storm
 
 filt_storms_info$Start_SpCond <- NaN
+filt_storms_info$End_SpCond <- NaN
+filt_storms_info$Flux_SpCond <- NaN
 filt_storms_info$uWTemp <- NaN
-filt_storms_info$Max_Dis <- NaN
+filt_storms_info$MaxDis <- NaN
+filt_storms_info$yday <- NaN
+
 
 for( ID in unique(filt_storms_info$Storm_ID)){
   
   stormdata <- filter( B1, ID == Storm_ID )
   
   filt_storms_info[ ID == filt_storms_info$Storm_ID, "Start_SpCond" ] <- stormdata$SpCond_uScm[1]
+  filt_storms_info[ ID == filt_storms_info$Storm_ID, "End_SpCond" ] <- stormdata$SpCond_uScm[nrow(stormdata)]
+  filt_storms_info[ ID == filt_storms_info$Storm_ID, "Flux_SpCond" ] <- 
+    (stormdata$SpCond_uScm[1] - stormdata$SpCond_uScm[nrow(stormdata)] )
   filt_storms_info[ ID == filt_storms_info$Storm_ID, "uWTemp" ] <- mean( stormdata$Temp_degC )
   filt_storms_info[ ID == filt_storms_info$Storm_ID, "MaxDis" ] <- max( stormdata$Discharge )
+  filt_storms_info[ ID == filt_storms_info$Storm_ID, "MinMax_SpCond" ] <-
+      max(stormdata$SpCond_uScm) - min(stormdata$SpCond_uScm)
+  filt_storms_info[ ID == filt_storms_info$Storm_ID, "yday" ] <- unique(yday(stormdata$Storm_ID))[1]
 }
+
+rm(stormdata, ID)
 
 write_csv(B1,"B1_WQ.csv")
 write_csv(filt_storms_info,"filt_storms_info.csv")
